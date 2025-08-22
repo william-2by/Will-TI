26ce226f6c3be2edf91db97b7a43820344e5d74a
william-2by <williamdim@hotmail.com>
2025-08-22 14:21:33 -0300

    add readme

---
+++ JS/tiposDeDados.js
@@ -0,0 +1,64 @@
+/*
+
+1. Strings -> Textos. (Quando eu quero escrever o tipo de dado string(textos) devemos usar 
+"aspas duplas"
+'aspas simples'
+´crase´ textos com a crase que inclusive pode pular linhas, quantas quiser. ) 
+
+const texto01 = "textos com aspas duplas"
+const texto02 = 'textos com aspas simples'
+const texto03 = `textos com crases`//template litarals
+
+console.log(texto01)
+console.log(texto02)
+console.log(texto03)
+
+const myAge = 30
+const myCar = 'Audi'
+
+const myString = `Minha idade é ${myAge}, tô ficando velho.
+Mas pelo menos tô andando de ${myCar}.`
+
+    
+
+console.log(myString)
+
+
+2.Numbers -> Números 
+
+
+
+const number1 = 32 / 2  //se colocar entre aspas vira um caractere "texto" como exemplo abaixo:
+const fakeNumber = "32 / 2"  //dessa forma o código iráentender que é uma string
+
+console.log(number1)
+console.log(fakeNumber)
+
+3. Boolean -> Booleano se comunica como TRUE ou FALSE (Verdadeiro ou falso)
+
+4. Object -> Objetos pode guardar quantas propriedades quiser
+
+5. Null / Undefined -> Null opeção do programador de deixar vazio. Undefined é quando tento execultar ou buscar alguma declaração que não existe.
+
+6. Estrutura de dados
+1. Array
+ex: const myArray = [20, 30, "Olá JS, { name: "Rodoufo", age: 30}"]
+
+Controlador de Fluxo 
+- IF (Se)
+- Else (Se não)
+
+Operadores de comparação
+- > Maior que
+- < Menor que
+- ==Igual que
+
+
+
+
+
+*/
+
+
+
+
---
+++ JS/variáveis.js
@@ -0,0 +1,8 @@
+/*
+Variáveis são espaçinhos de memórias onde ultilizamos pra armazenar algo dentro.
+Existem 3 no JS:
+ 
+* let (não pode: começar com números, começar com letra maiúscula) pode ir trocando o valor sem problemas.
+* const - não pode ser declarado novamente.
+* var - Descontinuado
+ */
